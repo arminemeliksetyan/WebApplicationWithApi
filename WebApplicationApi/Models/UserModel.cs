215c0f7ae7e0c2053955fbf0f2616a850fa668bd
armine.meliksetyan.1996@gmail.com <armine.meliksetyan.1996@gmail.com>
2023-08-01 19:00:13 +0400

    Add project files.

---
+++ WebApplicationApi/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationApi.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(11)]
+        public string Phone { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(300)]
+        public string Login { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(300)]
+        public string Password { get; set; }
+    }
+}
