9bc90b726963647d1781b7ac638e88fbb1b507a9
armine.meliksetyan.1996@gmail.com <armine.meliksetyan.1996@gmail.com>
2023-08-02 23:05:31 +0400

    asp .net core api

---
+++ WebApplicationSocialMediaApi/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationSocialMediaApi.Models;
+
+namespace WebApplicationSocialMediaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult CreaateUser(UserAdd userAdd)
+        {
+            try
+            {
+                int x = 12;
+                int y = 13;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult FindUser(int id)
+        {
+            try
+            {
+                Dictionary<int, string> users = new Dictionary<int, string>();
+                users.Add(0, "Arine");
+                users.Add(1, "Armine");
+                users.Add(2, "Ann");
+                users.Add(3, "Albert");
+                if (users.ContainsKey(id))
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);   
+                throw;
+            }
+        }
+
+        [HttpDelete("DeleteThis")]
+        public ActionResult DeleteUser(int id)
+        {
+            try
+            {
+                Dictionary<int, string> users = new Dictionary<int, string>();
+                users.Add(0, "Arine");
+                users.Add(1, "Armine");
+                users.Add(2, "Ann");
+                users.Add(3, "Albert");
+                if (users.ContainsKey(id))
+                {
+                    users.Remove(id);
+                    return Ok($"User id {id}");
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ WebApplicationSocialMediaApi/Models/UserAdd.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationSocialMediaApi.Models
+{
+    public class UserAdd
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Number { get; set; }
+        public bool Gender { get; set; }
+    }
+}
